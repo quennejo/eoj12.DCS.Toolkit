8f9220202bbb30047092c037fdafd982d302fbc2
Joé Quenneville <joe.quenneville@gmail.com>
2024-07-19 16:10:37 -0400

    Add Definition file #2 for admin only

--- eoj12.DCS.Toolkit/Models/Settings.cs
+++ eoj12.DCS.Toolkit/Models/Settings.cs
@@ -10,6 +10,9 @@ namespace eoj12.DCS.Toolkit.Models
     {
         public string DCSSaveGamesPath { get; set; }
         public string SquadronUrl{ get; set; }
+        public string SquadronDesctiption { get; set; }
+        public string SquadronUrl2 { get; set; }
+        public string SquadronDesctiption2 { get; set; }
         public string LogoBase64 { get; set; }
         public string LogoName { get; set; }
         public bool IsAdmin { get; set; }
